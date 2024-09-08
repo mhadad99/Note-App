c24d1317c9e78fa47128577c09abcd44535ca0dc
mhadd <mhadad315@gmail.com>
2024-09-08 21:54:51 +0300

    build Notes Items

---
+++ lib/views/widgets/note_item.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        top: 16,
+        left: 8,
+        bottom: 16,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              "Note Title",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: Text(
+                "Build Your App With me Haddad",
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.5),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(FontAwesomeIcons.trash),
+              color: Colors.black,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0, top: 16),
+            child: Text(
+              "21, May, 2022",
+              style: TextStyle(
+                color: Colors.black.withOpacity(.5),
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'notes_list_view.dart';
+
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Expanded(child: NotesListView()),
+        ],
+      ),
+    );
   }
 }
