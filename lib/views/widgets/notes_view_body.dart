ee7ff189af737c386830f64e1af6435538059a23
mhadd <mhadad315@gmail.com>
2024-09-11 12:31:34 +0300

    add text if there is no notes

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../cubits/notes_cubit/notes_cubit.dart';
 import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -7,13 +9,19 @@ class NotesViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          Expanded(child: NotesListView()),
-        ],
-      ),
-    );
+    return BlocBuilder<NotesCubit, NotesState>(
+        builder: (context, state) => Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Expanded(
+                      child: BlocProvider.of<NotesCubit>(context).notes.isEmpty
+                          ? Center(
+                              child: Text("Try Add some Notes".toUpperCase()),
+                            )
+                          : const NotesListView()),
+                ],
+              ),
+            ));
   }
 }
