7492a193c2ae6f90f23e8758e9634e8338453f1e
mhadd <mhadad315@gmail.com>
2024-09-11 10:32:40 +0300

    Implement the search and the app is finished

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/models/note_model.dart';
-
-import 'note_item.dart';
+import 'package:note_app/views/widgets/note_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
